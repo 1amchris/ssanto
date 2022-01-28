d00071eb67d20fb4ed48e16d775fe68d8cebca35
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-01-28 09:59:57 -0500

    Removed npm files

---
+++ ssanto-demo-week2/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// bootstrap
+import "bootstrap5/src/css/bootstrap.min.css";
+import "bootstrap5/src/js/bootstrap.bundle.min.js";
+
+// Leaflet
+import "leaflet/dist/leaflet.css";
+import "leaflet/dist/leaflet.js";
+
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
