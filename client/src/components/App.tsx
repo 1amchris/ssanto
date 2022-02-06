962e7869b65cdeab6b212561fa68cc44296414de
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-02-06 11:05:52 -0500

    Created a collapsible item

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,6 +3,7 @@ import Counter from './Counter';
 import MenuBar from './menu-bar/MenuBar';
 import { withTranslation, Trans } from 'react-i18next';
 import NavigationBar from './navigation-bar/NavigationBar';
+import Collapsible from './collapsible/Collapsible';
 
 function App({ t }: any) {
   const file = 'src/App.tsx';
@@ -15,9 +16,60 @@ function App({ t }: any) {
       <div className="d-grid" style={{ gridTemplateColumns: '270px auto' }}>
         <aside>
           <NavigationBar>
-            <span>field 1</span>
-            <span>field 2</span>
-            <span>field 3</span>
+            <Collapsible title={'Parameters & Settings'}>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod.
+              </p>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                single-origin coffee nulla assumenda shoreditch et. Nihil anim
+                keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                Leggings occaecat craft beer farm-to-table, raw denim aesthetic
+                synth nesciunt you probably haven't heard of them accusamus
+                labore sustainable VHS.
+              </p>
+            </Collapsible>
+            <Collapsible title={'Study area'}>
+              <div className="mb-3">
+                <label className="form-label">Default file input example</label>
+                <input className="form-control" type="file" id="formFile" />
+              </div>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod.
+              </p>
+            </Collapsible>
+            <Collapsible title={'NBS system type'}>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod.
+              </p>
+            </Collapsible>
+            <Collapsible title={'Objective hierarchy'}>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Anim pariatur cliche reprehenderit, enim eiusmod high life
+                accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                non cupidatat skateboard dolor brunch. Food truck quinoa
+                nesciunt laborum eiusmod.
+              </p>
+              <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid
+                single-origin coffee nulla assumenda shoreditch et. Nihil anim
+                keffiyeh helvetica, craft beer labore wes anderson cred nesciunt
+                sapiente ea proident. Ad vegan excepteur butcher vice lomo.
+                Leggings occaecat craft beer farm-to-table, raw denim aesthetic
+                synth nesciunt you probably haven't heard of them accusamus
+                labore sustainable VHS.
+              </p>
+            </Collapsible>
           </NavigationBar>
         </aside>
         <main className="container mt-3">
---
+++ client/src/components/collapsible/Collapsible.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { withTranslation } from 'react-i18next';
+import { FcCollapse, FcExpand } from 'react-icons/fc';
+import { uniqueId } from 'lodash';
+import { textChangeRangeIsUnchanged } from 'typescript';
+
+class Collapsible extends React.Component {
+  t: (str: string) => string;
+  title: string;
+  id: string;
+
+  state = {
+    open: true,
+  };
+
+  constructor(props: any) {
+    super(props);
+    const { t, title } = props;
+    this.t = t;
+    this.title = title;
+    this.id = uniqueId('collapsible-');
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div
+          className="w-100 d-flex justify-content-between"
+          data-bs-toggle="collapse"
+          data-bs-target={`#${this.id}`}
+          role="button"
+          aria-expanded="true"
+          aria-controls={this.id}
+          onClick={() => {
+            this.setState({ open: !this.state.open });
+          }}
+        >
+          <h5>{this.t(this.title || 'collapsible-title')}</h5>
+          <span>{this.state.open ? <FcCollapse /> : <FcExpand />}</span>
+        </div>
+        <div className="collapse show" id={this.id}>
+          {this.props?.children}
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default withTranslation()(Collapsible);
