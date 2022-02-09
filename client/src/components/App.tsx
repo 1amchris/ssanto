ee2703120d45a7a926229fc0612e32168ee2fbd9
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-02-08 19:02:53 -0500

    Created basic right aside cards for date

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -4,6 +4,7 @@ import MenuBar from './menu-bar/MenuBar';
 import { withTranslation, Trans } from 'react-i18next';
 import NavigationBar from './navigation-bar/NavigationBar';
 import Collapsible from './collapsible/Collapsible';
+import Data from './data/Data';
 
 function App({ t }: any) {
   // const file = 'src/App.tsx';
@@ -89,24 +90,22 @@ function App({ t }: any) {
               'World population',
               'Vertical bar charts',
               'Economics & politics',
-            ].map((title: string) => (
-              <article key={title} className="card mb-2">
-                <section className="card-body">
-                  <Collapsible title={title}>
-                    <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
-                      accusamus terry richardson ad squid. 3 wolf moon officia
-                      aute, non cupidatat skateboard dolor brunch. Food truck
-                      quinoa nesciunt laborum eiusmod.
-                    </p>
-                    <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
-                      Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
-                      probably haven't heard of them accusamus labore
-                      sustainable VHS.
-                    </p>
-                  </Collapsible>
-                </section>
-              </article>
+            ].map((title: string, index: number) => (
+              <Data key={`data-${index}`}>
+                <Collapsible title={title}>
+                  <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life
+                    accusamus terry richardson ad squid. 3 wolf moon officia
+                    aute, non cupidatat skateboard dolor brunch. Food truck
+                    quinoa nesciunt laborum eiusmod.
+                  </p>
+                  <p style={{ textAlign: 'justify', textIndent: '2rem' }}>
+                    Brunch 3 wolf moon tempor, sunt aliqua put a bird on it
+                    probably haven't heard of them accusamus labore sustainable
+                    VHS.
+                  </p>
+                </Collapsible>
+              </Data>
             ))}
           </aside>
         </main>
--- client/src/components/navigation-bar/NavigationBar.tsx
+++ client/src/components/navigation-bar/NavigationBar.tsx
@@ -1,13 +1,17 @@
 import React, { ReactElement } from 'react';
 
-function NavigationBar({ children }: any): ReactElement {
+function NavigationBar(
+  { children, className }: any,
+  key?: string
+): ReactElement {
   return (
     <nav
-      className="border-end bg-light overflow-scroll"
+      key={key}
+      className={`border-end bg-light overflow-scroll ${className}`}
       style={{ height: 'calc(100vh - 24px)', width: '270px' }}
     >
       <ul className="list-unstyled m-3">
-        {children?.map((child: ReactElement, index: number) => (
+        {[].concat(children).map((child: ReactElement, index: number) => (
           <li
             key={`navigation/item-${index}`}
             className="pb-2 border-bottom mb-2"
