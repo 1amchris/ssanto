6e43ee3940a5bfe12f36f9b48954764efb511066
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-02-18 17:59:14 -0500

    Updated (and renamed to electron.js) main.js

---
+++ electron.js
@@ -0,0 +1,69 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const { spawn } = require('child_process');
+
+const python = spawn('./venv/bin/python3.9', ['-u', './main.py'], {
+  cwd: path.join(__dirname, 'server'),
+});
+
+python.stdout.on('data', data => {
+  process.stdout.write(`[Python][stdout] ${data}`);
+});
+
+python.stderr.on('data', data => {
+  console.log(`[Python][stderr] ${data}`);
+});
+
+python.on('error', error => {
+  console.log(`[Python][error] Error: ${error}`);
+});
+
+python.on('close', code => {
+  console.log(`[Python][close] Code: ${code}`);
+});
+
+python.on('exit', (code, signal) => {
+  console.log(`[Python][exit] Code: ${code} Signal: ${signal}`);
+});
+
+/* ----------------------*/
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow = null;
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({
+    title: 'SSanto',
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      frame: false,
+    },
+  });
+
+  //mainWindow.maximize();
+  mainWindow.loadURL(
+    'http://localhost:3000'
+    /*url.format({
+            pathname: path.join(__dirname, './index.html'),
+            protocol: "file:",
+            slashes: true
+        })*/
+  );
+  mainWindow.webContents.openDevTools();
+
+  mainWindow.on('closed', function () {
+    console.log('electron exit');
+    python.kill();
+  });
+});
+
+app.on('window-all-closed', function () {
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
