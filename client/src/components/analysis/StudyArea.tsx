f31220dd661717e7adb405651522f6ae1f095b76
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-02-23 14:44:12 -0500

    removed unused import

--- client/src/components/analysis/StudyArea.tsx
+++ client/src/components/analysis/StudyArea.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { capitalize } from 'lodash';
 import { withTranslation } from 'react-i18next';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
