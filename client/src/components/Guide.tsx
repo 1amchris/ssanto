be40fc7fc4ed098118a18ee74f9aa1b1e60bce03
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-03-10 22:42:49 -0500

    Added markdown converter

--- client/src/components/Guide.tsx
+++ client/src/components/Guide.tsx
@@ -4,41 +4,86 @@ import { FcPrevious } from 'react-icons/fc';
 import { Link } from 'react-router-dom';
 import { capitalize } from 'lodash';
 import MenuBar from 'components/menu-bar/MenuBar';
-import FormsBar from 'components/forms-bar/FormsBar';
+import ReactMarkdown from 'react-markdown';
 
 function Guide({ t }: any) {
+  const markdown = `A paragraph with *emphasis* and **strong importance**.
+
+  > A block quote with \`a little bit of code\` and a URL: https://reactjs.org.
+  
+  * Lists
+  * [ ] todo
+  * [x] done
+  
+  A table:
+  
+  | a | b |
+  | - | - |
+  `;
+
+  const categories = [
+    'home',
+    'getting started',
+    'managing results',
+    'building a model',
+    'getting analytics',
+    'personalization',
+    'monte carlo analysis',
+  ];
+
+  const children = [
+    <Link to="/" className="text-decoration-none text-muted small">
+      <FcPrevious className="me-1" />
+      <span>{capitalize(t('return'))}</span>
+    </Link>,
+    <h5>Documentation</h5>,
+    <ul className="list-unstyled ms-3">
+      {categories.map((category: string, index: number) => (
+        <li key={`guide/category-${index}`}>
+          <a href={`#${category}`} className="text-reset text-decoration-none">
+            {capitalize(t(category))}
+          </a>
+        </li>
+      ))}
+    </ul>,
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="Guide">
+      <header className="Guide-header">
         <MenuBar />
       </header>
-      <div className="d-grid" style={{ gridTemplateColumns: '270px auto' }}>
-        <aside>
-          <FormsBar>
-            <Link to="/" className="text-decoration-none text-muted small">
-              <FcPrevious className="me-1" />
-              <span>{capitalize(t('return'))}</span>
-            </Link>
-            <h5>Documentation</h5>
-            <ul className="list-unstyled ms-3">
-              {[
-                'home',
-                'getting started',
-                'managing results',
-                'building a model',
-                'getting analytics',
-                'personalization',
-                'monte carlo analysis',
-              ].map((category: string, index: number) => (
-                <li key={`guide/category-${index}`}>
-                  {capitalize(t(category))}
+      <div
+        className="d-grid"
+        style={{
+          gridTemplateColumns: '270px auto',
+          height: 'calc(100vh - 24px)',
+        }}
+      >
+        <aside className="h-100">
+          <nav className={`border-end bg-light overflow-scroll h-100`}>
+            <ul className="list-unstyled m-3">
+              {children.map((child, index: number) => (
+                <li
+                  key={`navigation/item-${index}`}
+                  className="pb-2 border-bottom mb-2"
+                >
+                  {child}
                 </li>
               ))}
             </ul>
-          </FormsBar>
+          </nav>
         </aside>
-        <main className="shadow container mt-3" style={{ zIndex: 1 }}>
-          This is a guide :)
+        <main
+          className="shadow container overflow-scroll"
+          style={{ zIndex: 1, height: 'calc(100vh - 24px)' }}
+        >
+          {categories.map((category: any) => (
+            <article id={category} key={`category/${category}`}>
+              <h1>{category}</h1>
+              <ReactMarkdown>{markdown}</ReactMarkdown>
+            </article>
+          ))}
         </main>
       </div>
     </div>
