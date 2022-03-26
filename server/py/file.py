3d3e6af87c3b99181c7460a49fadb98a6a851880
Tristan Rioux <tristan.rioux@polymtl.ca>
2022-03-26 19:12:52 -0400

    Shapefile object added
    Former-commit-id: 4df8b93ec58249178ce3f138f7658283a8c76c68

--- server/py/file.py
+++ server/py/file.py
@@ -1,27 +1,11 @@
-import string
-import geopandas
 from io import BytesIO
 from py.file_metadata import FileMetaData
 
 
 class File(FileMetaData):
-    def __init__(self, name: string, content: bytes, id=None, group_id=None):
+    def __init__(self, name: str, content: bytes, id=None, group_id=None):
         super().__init__(name, id=id, group_id=group_id)
         self.content = content
-        ''' Not used?
-        self.columns = []
-        self.head = []
-
-    def set_head(self):
-        df = geopandas.read_file(self.path[0])
-        self.head = df.loc[:, df.columns != "geometry"].head(5).to_dict("index")
-        print(self.head)
-
-    def set_column(self):
-        df = geopandas.read_file(self.path[0])
-        self.columns = [s for s in df.columns if s != "geometry"]
-        print(self.columns)
-        '''
 
     def __dict__(self):
         base = super().__dict__()
