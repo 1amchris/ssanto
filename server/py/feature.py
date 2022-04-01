Solve bug with scaling value continuous; Remove default value :p
Former-commit-id: af4a04aa1ed59def17cb1d54ca299debad6f9d3b

server/py/feature.py
@@ -47,7 +47,7 @@ class ContinuousFeature(Feature):
         )
 
         self.field_name = field_name
-        self.scaling_function = "x"
+        self.scaling_function = scaling_function
 
     def update(self):
         print("update", self.path, "****", self.output_tiff)
@@ -83,8 +83,10 @@ class ContinuousFeature(Feature):
         )
 
         offset = offset = (
-            -int((origin_file[1] - self.study_area.origin[1]) // self.cell_size),
-            int((origin_file[0] - self.study_area.origin[0]) // self.cell_size),
+            -int((origin_file[1] - self.study_area.origin[1]
+                  ) // self.cell_size),
+            int((origin_file[0] - self.study_area.origin[0]) //
+                self.cell_size),
         )
         return self.balance_matrix(file, self.study_area.as_array, offset)
 
@@ -92,15 +94,15 @@ class ContinuousFeature(Feature):
         output_matrix = np.zeros(study_area.shape)
         output_matrix = np.zeros(study_area.shape)
         output_matrix[
-            max(offset[0], 0) : max(
+            max(offset[0], 0): max(
                 min(len(input_matrix) + offset[0], len(study_area)), 0
             ),
-            max(offset[1], 0) : max(
+            max(offset[1], 0): max(
                 min(len(input_matrix[0]) + offset[1], len(study_area[0])), 0
             ),
         ] = input_matrix[
-            max(0, -offset[0]) : max(len(study_area) - offset[0], 0),
-            max(0, -offset[1]) : max(len(study_area[0]) - offset[1], 0),
+            max(0, -offset[0]): max(len(study_area) - offset[0], 0),
+            max(0, -offset[1]): max(len(study_area[0]) - offset[1], 0),
         ]
         return output_matrix
 
@@ -334,6 +336,7 @@ class CategoricalFeature(ContinuousFeature):
     def categorize_values(self):
         df = geopandas.read_file(self.path)
         df["cal_value"] = (
-            df[self.field_name].map(self.categorized_value).fillna(0.0).astype(float)
+            df[self.field_name].map(
+                self.categorized_value).fillna(0.0).astype(float)
         )
         df.to_file(self.path)