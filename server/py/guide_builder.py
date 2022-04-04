61314eb6011767b029bb38d80ca6487de9999299
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-04-03 22:29:29 -0400

    Fixed cross-guide references
    Former-commit-id: 91a8c17afdea72320b028c1deec1ef1f6b5a8ddb

--- server/py/guide_builder.py
+++ server/py/guide_builder.py
@@ -1,22 +1,25 @@
 import os
 
+
 class Topic:
     def __init__(self, path, label):
-        self.name = 'topic_' + str(GuideBuilder.next_topic_id())
-        name_without_ext = os.path.splitext(label)[0]
-        self.label = name_without_ext.split('.', 1)[1]
+        stem = os.path.splitext(label)[0]
+        self.label = stem.split(".", 1)[1]
+        self.name = GuideBuilder.to_html_hash(self.label)
         path = os.path.join(path, label)
         with open(path) as t:
             self.content = t.read()
 
+
 class Category:
     def __init__(self, path, label):
-        self.name = 'category_' + str(GuideBuilder.next_category_id())
-        self.label = label.split('.', 1)[1]
+        self.label = label.split(".", 1)[1]
+        self.name = GuideBuilder.to_html_hash(self.label)
         self.topics = []
         path = os.path.join(path, label)
         for topic in sorted(os.listdir(path)):
             self.topics.append(Topic(path, topic))
+
 
 class GuideBuilder:
     topic_counter = 0
@@ -24,14 +27,14 @@ class GuideBuilder:
 
     def __init__(self):
         self.categories = []
-        path = os.path.join(os.getcwd(), 'guide')
+        path = os.path.join(os.getcwd(), "guide")
         if not os.path.exists(path):
             print("Path '{}' doesn't exit. The guide will be empty.".format(path))
             return
 
         for category in sorted(os.listdir(path)):
             self.categories.append(Category(path, category))
-    
+
     @staticmethod
     def next_topic_id():
         id = GuideBuilder.topic_counter
@@ -43,6 +46,10 @@ class GuideBuilder:
         id = GuideBuilder.category_counter
         GuideBuilder.category_counter += 1
         return id
+
+    @staticmethod
+    def to_html_hash(text):
+        return text.rsplit(".", 1)[0].replace(" ", "-")
 
     # WARN: Maybe we would want to reload the files from disk everytime ?
     def generate_guide_data(self):
