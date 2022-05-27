b5f783a95cec56b2a7cee97dc146827d80d15680
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-05-27 16:19:14 -0400

    Created editor group and groups
    Former-commit-id: 85dd5da3e45c153790a09cd7511aa407fbf6eb4a

---
+++ src/components/core/EditorGroup.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import DefaultView from 'components/core/DefaultView';
+
+function EditorGroup({ style }: any) {
+  return <DefaultView style={style} />;
+}
+
+export default EditorGroup;
---
+++ src/components/core/EditorGroups.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import EditorGroup from 'components/core/EditorGroup';
+import SplitView from 'components/core/SplitView';
+
+function EditorGroups({ style }: any) {
+  return (
+    <SplitView direction="row" style={style}>
+      <EditorGroup style={{ minWidth: 120 }} />
+      <EditorGroup style={{ minWidth: 120 }} />
+      <EditorGroup style={{ minWidth: 120 }} />
+    </SplitView>
+  );
+}
+export default EditorGroups;
