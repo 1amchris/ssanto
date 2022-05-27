9bc5b8a5c8f41cea04f300e1f57ed6838d5694e4
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-05-27 16:17:19 -0400

    Created a default view
    Former-commit-id: efb433b9ce3e62f062c6e7c9bfefd937766b58e3

---
+++ src/components/core/DefaultView.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { min } from 'lodash';
+import { useResizeDetector } from 'react-resize-detector';
+
+function DefaultView({ style, showTips = true }: any) {
+  const { width, height, ref } = useResizeDetector();
+  const maxSize = 175;
+  const size = min([width || 0, height || 0, maxSize])!;
+  const _showTips = showTips && height !== undefined && height > maxSize * 1.5;
+
+  return (
+    <div
+      style={{ width: '100%', height: '100%', ...style }}
+      className="d-flex flex-column justify-content-center align-items-center"
+      ref={ref}
+    >
+      <img
+        src="logo512_map.png"
+        className="p-3"
+        style={{
+          width: size,
+          height: size,
+          opacity: 0.65,
+          filter: 'grayscale(100%)',
+        }}
+        alt="SSANTO map logo"
+      />
+      {_showTips && (
+        <p className="d-flex flex-column text-secondary p-2">
+          <span className="p-1">
+            <kbd className="bg-secondary">CMD</kbd> +{' '}
+            <kbd className="bg-secondary">O</kbd> to open a project
+          </span>
+          <span className="p-1">
+            <kbd className="bg-secondary">CMD</kbd> +{' '}
+            <kbd className="bg-secondary">Q</kbd> to close SSanto
+          </span>
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default DefaultView;
