103ac81c9b0b47a734868688db06fc9f63c0f823
Christophe Beaulieu <beaulieu.christophe23@gmail.com>
2022-06-02 21:42:59 -0400

    Updated keyboard shortcuts
    Former-commit-id: 955b665edf7cc88e4adab34b02be236de76855c1

--- src/components/common/DefaultView.tsx
+++ src/components/common/DefaultView.tsx
@@ -6,7 +6,18 @@ function DefaultView({ style, showTips = true }: any) {
   const { width, height, ref } = useResizeDetector();
   const maxSize = 175;
   const size = min([width || 0, height || 0, maxSize])!;
-  const _showTips = showTips && height !== undefined && height > maxSize * 1.5;
+  const _showTips =
+    showTips &&
+    height !== undefined &&
+    height > maxSize * 1.5 &&
+    width !== undefined &&
+    width > maxSize * 1.5;
+
+  const tips = [
+    { label: 'Show all Commands', keys: ['SHIFT', 'CMD', 'P'] },
+    { label: 'Open a Project', keys: ['CMD', 'O'] },
+    { label: 'Close SSANTO', keys: ['CMD', 'Q'] },
+  ];
 
   return (
     <div
@@ -27,15 +38,21 @@ function DefaultView({ style, showTips = true }: any) {
         alt="SSANTO map logo"
       />
       {_showTips && (
-        <p className="d-flex flex-column text-secondary p-2">
-          <span className="p-1 text-center">
-            <kbd className="bg-secondary">CMD</kbd> +{' '}
-            <kbd className="bg-secondary">O</kbd> to open a project
-          </span>
-          <span className="p-1 text-center">
-            <kbd className="bg-secondary">CMD</kbd> +{' '}
-            <kbd className="bg-secondary">Q</kbd> to close SSanto
-          </span>
+        <p className="d-flex flex-column text-secondary small p-2">
+          {tips.map((tip, index) => (
+            <span className="p-1 row flex-nowrap" key={index}>
+              <span className="col-6 px-0 text-end me-2 text-nowrap">
+                {tip.label}
+              </span>
+              <span className="col-5 px-0 text-start">
+                {tip.keys.map((key, index) => (
+                  <kbd className="bg-secondary me-1" key={index}>
+                    {key}
+                  </kbd>
+                ))}
+              </span>
+            </span>
+          ))}
         </p>
       )}
     </div>
